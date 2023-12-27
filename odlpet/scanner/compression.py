a70742e78fc44b7e31b058bcf203155ddb2e598e
antonadelow <88186878+antonadelow@users.noreply.github.com>
2023-12-27 16:24:16 +0100

    Update compression.py

--- odlpet/scanner/compression.py
+++ odlpet/scanner/compression.py
@@ -173,8 +173,9 @@ class Compression:
 
     def get_stir_proj_data_info(self):
         _stir_scanner = self.scanner.get_stir_scanner()
-        proj_data_info = ProjDataInfo.ProjDataInfoGE(
+        proj_data_info = construct_proj_data_info(
             _stir_scanner,
+            self.span_num,
             self.max_diff_ring,
             self.num_of_views,
             self.get_num_tangential(),
